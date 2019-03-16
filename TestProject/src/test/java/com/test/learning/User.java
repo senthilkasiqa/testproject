70cf526dff650ce89fbc4b4e2af78c949486de65
Senthilkumar <senthilkasiqa@gmail.com>
2019-03-17 00:02:17 +0530

    intial commmit

---
+++ TestProject/src/test/java/com/test/learning/User.java
@@ -0,0 +1,25 @@
+package com.test.learning;
+
+public class User {
+
+    String userName;
+    String password;
+
+    public static void main(String args[]){
+        User user=new User();
+        user.User("senthil","password");
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void User(String userName, String password) {
+        this.userName=userName;
+        this.password=password;
+    }
+}
